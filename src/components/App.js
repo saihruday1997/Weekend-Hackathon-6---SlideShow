e4af64cca1fbbbcfd03fe5e1dacd195f4181d92c
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-22 02:55:46 +0530

    chngs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import slides from "../data.js";
-import Slides from "./Slides";
 
-const App = () => {
-  //console.log(slides);
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+  const allSlides = props.slidesList;
+  const length = allSlides.length;
+
+  let title = allSlides[index].title;
+  let text = allSlides[index].text;
+
   return (
     <>
-      <Slides slidesList={slides} />
+      <h1 data-testid="title">{title}</h1>
+      <p data-testid="text">{text}</p>
+      <button
+        data-testid="button-prev"
+        disabled={index === 0}
+        onClick={() => {
+          if (index !== 0) {
+            setIndex(index - 1);
+          }
+        }}
+      >
+        PREV
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={index === length - 1}
+        onClick={() => {
+          if (index !== length - 1) {
+            setIndex(index + 1);
+          }
+        }}
+      >
+        NEXT
+      </button>
+      <button
+        data-testid="button-restart"
+        disabled={index === 0}
+        onClick={() => setIndex(0)}
+      >
+        RESTART
+      </button>
     </>
   );
 };
