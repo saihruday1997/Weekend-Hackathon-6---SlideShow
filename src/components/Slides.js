9c87f8866aaf678918130d2417e352c68b4ba517
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-21 18:38:34 +0530

    chngs

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -2,13 +2,36 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const Slides = (props) => {
+  const [index, setIndex] = useState(0);
+  const [nextdis, setNextdis] = useState(false);
+  const [prevdis, setPrevdis] = useState(false);
+  const [resetdis, setResetdis] = useState(false);
+
   return (
     <>
-      <h1 data-testid="title">{props.slidesList[0].title}</h1>
-      <p data-testid="text">{props.slidesList[0].text}</p>
-      <button data-testid="button-prev">PREV</button>
-      <button data-testid="button-next">NEXT</button>
-      <button data-testid="button-restart">RESTART</button>
+      <h1 data-testid="title">{props.slidesList[index].title}</h1>
+      <p data-testid="text">{props.slidesList[index].text}</p>
+      <button
+        data-testid="button-prev"
+        disabled={prevdis}
+        onClick={setIndex(index - 1)}
+      >
+        PREV
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={nextdis}
+        onClick={setIndex(index + 1)}
+      >
+        NEXT
+      </button>
+      <button
+        data-testid="button-restart"
+        disabled={resetdis}
+        onClick={setIndex(0)}
+      >
+        RESTART
+      </button>
     </>
   );
 };
